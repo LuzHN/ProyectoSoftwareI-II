10304b5c445dc308b4bc7a779a2fc598f071967f
hrmendoza96 <hrmendoza96@unitec.edu>
2018-06-04 02:36:35 -0600

    UI Design de REGISTER

--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
+import '../imports/client/styles/register';
 
 //import '../imports/ui/body.js';
 //import '../imports/ui/login.css';
 import NotFound from '../imports/ui/NotFound';
 import Principal from '../imports/ui/Principal';
 import Register from '../imports/ui/Register';
+
 
 
 const routes = (
--- imports/ui/Register.js
+++ imports/ui/Register.js
@@ -24,41 +24,60 @@ export default class Register extends React.Component {
 
     render(){
         return (
-            <div className="container">
-                <div className="contact">
-                    <h3>Email Us</h3>
-                    <form>
-                        <p>
-                            <label>Email</label>
-                            <input type="email" placeholder='Enter Email' value={this.state.email}
-                            onChange={e => this.setState({email: e.target.value})} />
-                        </p>
-                        <p>
-                            <label>Password</label>
-                            <input type="password" placeholder='Enter Password' value={this.state.password} 
-                            onChange={e => this.setState({password: e.target.value})} />
-                        </p>
-                        <p>
-                            <label>Confirm Password</label>
-                            <input type="password" placeholder='Confirm Password' value={this.state.confirmPassword} 
-                            onChange={e => this.setState({confirmPassword: e.target.value})} />
-                        </p>
-                        <p>
-                            <label>First Name</label>
-                            <input type="text" placeholder='Enter First Name' value={this.state.firstName} 
-                            onChange={e => this.setState({firstName: e.target.value})} />
-                        </p>
-                        <p>
-                            <label>Last Name</label>
-                            <input type="text" placeholder='Enter Last Name' value={this.state.lastName} 
-                            onChange={e => this.setState({lastName: e.target.value})} />
-                        </p>
-                        <p>
-                            <button onClick={e => this.onSubmit(e)}>Register</button>    
-                        </p>
-                    </form>
+            <body className="container">
+                <div className="wrapper">
+                    <div className="contact">
+                    <div className="image"></div>​
+                        <form>
+                            <p>
+                                <label>Email</label>
+                                <input type="email" placeholder='Enter Email' value={this.state.email}
+                                onChange={e => this.setState({email: e.target.value})} />
+                            </p>
+                           
+                            <div className="container-1">
+                                <div className="box-1">
+                                    <p>
+                                        <label>Password</label>
+                                        <input type="password" placeholder='Enter Password' value={this.state.password} 
+                                        onChange={e => this.setState({password: e.target.value})} />
+                                    </p>
+                                </div>
+                                <div className="box-2">
+                                    <p>
+                                        <label>Confirm Password</label>
+                                        <input type="password" placeholder='Confirm Password' value={this.state.confirmPassword} 
+                                        onChange={e => this.setState({confirmPassword: e.target.value})} />
+                                    </p>
+                                </div>
+                            </div>
+                           
+                            <div className="container-1">
+
+                                <div className="box-1">
+                                    <p>
+                                        <label>First Name</label>
+                                        <input type="text" placeholder='Enter First Name' value={this.state.firstName} 
+                                        onChange={e => this.setState({firstName: e.target.value})} />
+                                    </p>
+                                </div>
+                                <div className="box-2">
+                                    <p>
+                                        <label>Last Name</label>
+                                        <input type="text" placeholder='Enter Last Name' value={this.state.lastName} 
+                                        onChange={e => this.setState({lastName: e.target.value})} />
+                                    </p>
+                                </div>
+                            </div>
+                            
+                            <p>
+                                <button onClick={e => this.onSubmit(e)}>Register</button>    
+                            </p>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </body>
+            
             
         );
     }
