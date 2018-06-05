d6773fabed25bfdb1f6f78ef8aeece99732241f5
josefernandez98 <jrfo_04@unitec.edu>
2018-06-05 07:44:33 -0600

    Cambios a login

--- imports/ui/LoginPage.js
+++ imports/ui/LoginPage.js
@@ -2,21 +2,42 @@ import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { disconnect } from 'cluster';
+import PropTypes from 'prop-types';
+import { Router, Route, browserHistory} from 'react-router';
+import { Redirect } from 'react-router'
 import './../client/styles/Login';
 
 export default class LoginPage extends React.Component {
-    userData = {
-        email : '',
-        password: ''
+    
+    state = {
+        redirect: true
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        console.log(this.userData);
-        this.setState({
-            email : '',
-            password: ''
-        });
+    }
+
+
+    onRegister() {   
+        const { redirect } = this.state;
+        if (redirect) {
+            return <Redirect to="/register"/>;
+        }
+    }
+
+    onLogin() {
+        /*Metodo para el boton Login*/ 
+        if (document.getElementById("emailBox").value != "" && document.getElementById("passwordBox").value != "") {
+            document.getElementById("emailBox").value = "";
+            document.getElementById("passwordBox").value = "";
+        } 
+        if (document.getElementById("passwordBox").value == "" && document.getElementById("emailBox").value == ""){
+            alert("Por favor ingrese sus datos.");
+        } else if (document.getElementById("emailBox").value == "") {
+            alert("Por favor ingrese su correo.");
+        } else if (document.getElementById("passwordBox").value == "") {
+            alert("Por favor ingrese su contraseña.");
+        } 
     }
 
     render() {
@@ -27,13 +48,17 @@ export default class LoginPage extends React.Component {
                         <div className = "Image"></div>
 
                         <label className = "emailLabel">Email</label>
-                        <input className = "emailInput" placeholder = "Email" type = "email" />
+                        <input className = "emailInput" placeholder = "Email" id = "emailBox"type = "email"/>
 
                         <label className = "passwordLabel">Password</label>
-                        <input className = "passwordInput" placeholder = "Password" type = "password"/>
+                        <input className = "passwordInput" placeholder = "Password" id = "passwordBox" type = "password"/>
 
-                        <button className = "loginButton">Login</button>
-                    
+                        <div className = "Buttons">
+                            <button className = "loginButton" onClick = {this.onLogin.bind(this)}>Login</button>
+                            <label className = "orLabel">OR</label>
+                            <button className = "registerButton" onClick = {this.onRegister.bind(this)}>Register</button>
+                        </div>
+
                 </form>
             </div>
         );
