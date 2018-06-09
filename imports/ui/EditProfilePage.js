26536b84b16ad880d163208ffa2fbd226c1ee085
josefernandez98 <jrfo_04@unitec.edu>
2018-06-09 17:07:44 -0600

    Se creo el EditProfilePage pero se hace pull de lo que subio Harold

--- imports/ui/EditProfilePage.js
+++ imports/ui/EditProfilePage.js
@@ -18,25 +18,55 @@ export default class EditProfilePage extends React.Component {
         return (
             <div className = "principalDiv">
                 <form className = "editProfileForm" onSubmit = {this.handleSubmit.bind(this)}>
-                    <div className = "container">
 
-                        <div className = "editNamesBox">
+                    {/*First Name and Last Name inputs and labels.*/}
+                    <div className = "editNamesBox">
+                        <div className = "firstNameBox">
                             <label className = "firstNameLabel">First Name</label>
-                            <input className = "firstNameInput" id = "firstNameId"/>   
+                            <input className = "firstNameInput" id = "firstNameId"/> 
+                        </div>
+                        <div className = "lastNameBox">
                             <label className = "lastNameLabel">Last Name</label>
-                            <input className = "lastNameInput" id = "passwordBox" />                 
-                        </div> {/*Aqui termina el div editNamesBox */}
+                            <input className = "lastNameInput" id = "passwordBox" /> 
+                        </div> 
+                    </div>
+                    {/*First Name and Last Name inputs and labels ends here.*/}
 
+                    {/*Old Password input and label.*/}
+                    <div className = "editNamesBox">
                         <div className = "oldPasswordBox">
                             <label className = "oldPasswordLabel">Old Password</label>
                             <input className = "oldPasswordInput" id = "oldPasswordId"/>
                         </div>
+                    </div>
+                    {/*Old Password input and label ends here.*/}
+
+                    {/*New and Confirm Password inputs and labels.*/}
+                    <div className = "editNamesBox">
+                        <div className = "firstNameBox">
+                            <label>New Password</label>
+                            <input id = "newPasswordId"/> 
+                        </div>
+                        <div className = "lastNameBox">
+                            <label>Confirm Password</label>
+                            <input id = "confirmPasswordId"/> 
+                        </div> 
+                    </div>
+                    {/*New and Confirm Password inputs and labels end here.*/}
+
+                    {/*New and Confirm Password inputs and labels.*/}
+                    <div className = "editNamesBox">
+                        <div className = "directionBox">
+                            <label>Dirección</label>
+                            <textarea id = "directionTextArea" maxLength='140' rows="5"/> 
+                        </div>
+                        <div className = "directionButtonBox">
+                            <button>Agregar Dirección</button>
+                        </div> 
+                    </div>
+                    {/*New and Confirm Password inputs and labels end here.*/}
 
 
-
-
-
-                    </div> {/*Aqui termina el div Container*/}
                 </form> 
             </div>
         );
