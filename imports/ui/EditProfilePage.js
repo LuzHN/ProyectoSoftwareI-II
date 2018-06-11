b1130ef61c7519ac0e09a8e1aba13de5bb34a272
hrmendoza96 <hrmendoza96@unitec.edu>
2018-06-11 17:07:12 -0600

    Avances Modify User

--- imports/ui/EditProfilePage.js
+++ imports/ui/EditProfilePage.js
@@ -19,77 +19,54 @@ export default class EditProfilePage extends React.Component {
         return (
             <div className = "principalDiv">
                 <form className = "editProfileForm" onSubmit = {this.handleSubmit.bind(this)}>
-
+                <h1>Editar Perfil</h1>
                     {/*First Name and Last Name inputs and labels.*/}
                     <div className = "containerBox">
                         <div className = "leftContainerBox">
                             <label>First Name</label>
-                            <input id = "firstNameId"/> 
+                            <input id = "firstNameId" maxLength='140' placeholder='Enter First Name'/> 
                         </div>
                         <div className = "rightContainerBox">
                             <label>Last Name</label>
-                            <input id = "passwordBox" /> 
+                            <input id = "passwordBox" maxLength='140' placeholder='Enter Last Name'/> 
                         </div> 
                     </div>
                     {/*First Name and Last Name inputs and labels ends here.*/}
 
                     {/*Old Password input and label.*/}
                     <div className = "passBox">
-                        <div className = "oldPasswordBox">
-                            <label>Old Password</label>
-                            <input id = "oldPasswordId"/>
-                        </div>
+                        
                     </div>
                     {/*Old Password input and label ends here.*/}
 
                     {/*New and Confirm Password inputs and labels.*/}
-                    <div className = "containerBox">
+                    <div className = "passBox">
+
+                        <div className = "oldPasswordBox">
+                            <label>*Old Password</label>
+                            <input type="password" id = "oldPasswordId" placeholder='Old Password'/>
+                        </div>
+
                         <div className = "leftContainerBox">
-                            <label>New Password</label>
-                            <input id = "newPasswordId"/> 
+                            <label>*New Password</label>
+                            <input type="password" id = "newPasswordId" placeholder='New Password'/> 
                         </div>
                         <div className = "rightContainerBox">
-                            <label>Confirm Password</label>
-                            <input id = "confirmPasswordId"/> 
+                            <label>*Confirm Password</label>
+                            <input type="password" id = "confirmPasswordId" placeholder='Confirm Password'/> 
                         </div> 
                     </div>
                     {/*New and Confirm Password inputs and labels end here.*/}
 
-                    {/*First two directions text areas.*/}
-                    <div className = "containerBox">
-                        <div className = "leftContainerBox">
-                            <label>Dirección 1</label>
-                            <textarea id = "direction1TextArea" maxLength='140' rows="5"/> 
-                        </div>
-                        <div className = "rightContainerBox">
-                            <label>Dirección 2</label>
-                            <textarea id = "direction2TextArea" maxLength='140' rows="5"/> 
-                        </div>
-                    </div>
-                    {/*First two directions text areas end here.*/}
-
-                    {/*Last two directions text areas.*/}
-                    <div className = "containerBox">
-                        <div className = "leftContainerBox">
-                            <label>Dirección 3</label>
-                            <textarea id = "direction3TextArea" maxLength='140' rows="5"/> 
-                        </div>
-                        <div className = "rightContainerBox">
-                            <label>Dirección 4</label>
-                            <textarea id = "direction4TextArea" maxLength='140' rows="5"/> 
-                        </div>
-                    </div>
-                    {/*Last two directions text areas end here.*/}
-
-                     {/*First two phone numbers.*/}
+                    {/*First two phone numbers.*/}
                      <div className = "containerBox">
                         <div className = "leftContainerBox">
-                            <label>Teléfono 1</label>
-                            <InputMask mask="9999-9999"/>
+                            <label>*Phone Number 1</label>
+                            <InputMask mask="9999-9999" placeholder='Enter Phone Number'/>
                         </div>
                         <div className = "rightContainerBox">
-                            <label>Teléfono 2</label>
-                            <InputMask mask="9999-9999"/>
+                            <label>Phone Number 2</label>
+                            <InputMask mask="9999-9999" placeholder='Enter Phone Number'/>
                         </div>
                     </div>
                     {/*First two phone numbers end here.*/}
@@ -97,15 +74,41 @@ export default class EditProfilePage extends React.Component {
                     {/*Last two phone numbers.*/}
                     <div className = "containerBox">
                         <div className = "leftContainerBox">
-                            <label>Teléfono 3</label>
-                            <InputMask mask="9999-9999"/>
+                            <label>Phone Number 3</label>
+                            <InputMask mask="9999-9999" placeholder='Enter Phone Number'/>
                         </div>
                         <div className = "rightContainerBox">
-                            <label>Teléfono 4</label>
-                            <InputMask mask="9999-9999"/>
+                            <label>Phone Number 4</label>
+                            <InputMask mask="9999-9999" placeholder='Enter Phone Number'/>
                         </div>
                     </div>
                     {/*Last two phone numbers end here.*/}
+
+                    {/*First two directions text areas.*/}
+                    <div className = "containerBox">
+                        <div className = "leftContainerBox">
+                            <label>*Address 1</label>
+                            <textarea id = "direction1TextArea" maxLength='140' rows="5" placeholder='Enter Address'/> 
+                        </div>
+                        <div className = "rightContainerBox">
+                            <label>Address 2</label>
+                            <textarea id = "direction2TextArea" maxLength='140' rows="5" placeholder='Enter Address'/> 
+                        </div>
+                    </div>
+                    {/*First two directions text areas end here.*/}
+
+                    {/*Last two directions text areas.*/}
+                    <div className = "containerBox">
+                        <div className = "leftContainerBox">
+                            <label>Address 3</label>
+                            <textarea id = "direction3TextArea" maxLength='140' rows="5" placeholder='Enter Address'/> 
+                        </div>
+                        <div className = "rightContainerBox">
+                            <label>Address 4</label>
+                            <textarea id = "direction4TextArea" maxLength='140' rows="5" placeholder='Enter Address'/> 
+                        </div>
+                    </div>
+                    {/*Last two directions text areas end here.*/}
 
                      <div className = "Buttons">
                             <button className = "disableAccBtn">Disable Account</button>
