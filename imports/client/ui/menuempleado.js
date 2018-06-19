5ccc95246d21755938c2cd915a95a8cb1845f23a
LuzHN <coldblitz@unitec.edu>
2018-06-19 16:58:29 -0600

    botones

--- imports/client/ui/menuempleado.js
+++ imports/client/ui/menuempleado.js
@@ -24,7 +24,7 @@ export default class MenuEmpleado extends React.Component {
                 <div className="pos-f-t "></div>
 
                 <section class="MenuEmpleado" >
-                    <MenuEmpleadoComponente/>
+                    <MenuEmpleadoComponente />
                 </section>
 
                 <img id="ColorStrip" src="http://www.healthkitchen.hn/static/media/color-strip.9c28b147.svg" />
@@ -52,45 +52,66 @@ export default class MenuEmpleado extends React.Component {
 
 }
 
-function Plato(categoria, titulo, precio, descripcion, foto, nutricional) {
+function Plato(categoria, titulo, precio, descripcion, foto, nutricional, estado) {
     this.categoria = categoria;
     this.titulo = titulo;
     this.precio = precio;
     this.descripcion = descripcion;
     this.foto = foto;
     this.nutricional = nutricional;
-  }
+    this.estado = estado;
+}
 
 var Entrees = [
     new Plato("Entree", "Cauliflower Nuggets", "129", "Empanizado con panco acompañado "
-      + "con una salsa fresca de tomate y Tzatziki", "http://cdn1-www.momtastic.com/assets/uploads/2016/06/Cauliflower-Nuggets-4.jpg", "")
+        + "con una salsa fresca de tomate y Tzatziki", "http://cdn1-www.momtastic.com/assets/uploads/2016/06/Cauliflower-Nuggets-4.jpg", "", "Pendiente")
     , new Plato("Entree", "Montaditos", "99", "Cuatro tostadas de pan de hierbas; atún, vegetales asados, pollo al pesto, y carne de berenjena",
-      "https://www.philadelphia.com.mx/modx/assets/img/revision2016/images/recetas/montaditos_fuerza_roja.jpg", "")
+        "https://www.philadelphia.com.mx/modx/assets/img/revision2016/images/recetas/montaditos_fuerza_roja.jpg", "", "Pendiente")
     , new Plato("Entree", "Croquetas de Vegetales", "99", "Fritura de carne de berenjena, papa y zanahoria rellos de cuajada y acompañados de Tatziki",
-      "https://www.hogarmania.com/archivos/201105/193-croquetas-de-verduras-y-queso-xl-668x400x80xX.jpg", "")
+        "https://www.hogarmania.com/archivos/201105/193-croquetas-de-verduras-y-queso-xl-668x400x80xX.jpg", "", "Pendiente")
     , new Plato("Entree", "Palitos de Camote", "49", "Camotes a la francesa, acompañado de aderezo Tzatziki.",
-      "http://www.contigosalud.com/files/images/Palitos%20camote%20francesa.jpg", "")
+        "http://www.contigosalud.com/files/images/Palitos%20camote%20francesa.jpg", "", "Pendiente")
     , new Plato("Entree", "Aros de Cebolla HK", "89", "5 aros de cebolla rellenos con pure de camote, guacamole y carne de berenjena y Empanizado con panco.",
-      "http://mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-2.jpg", "")
-  ];
+        "http://mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-2.jpg", "", "Pendiente")
+
+];
 
 
-  class MenuEmpleadoComponente extends React.Component{
-      render(){
-          return(
+class MenuEmpleadoComponente extends React.Component {
+
+    state = {
+        simon: "simon",
+        entradas: [{"categoria":"Entree","titulo":"Cauliflower Nuggets","precio":"129","descripcion":"Empanizado con panco acompañado con una salsa fresca de tomate y Tzatziki","foto":"http://cdn1-www.momtastic.com/assets/uploads/2016/06/Cauliflower-Nuggets-4.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Montaditos","precio":"99","descripcion":"Cuatro tostadas de pan de hierbas; atún, vegetales asados, pollo al pesto, y carne de berenjena","foto":"https://www.philadelphia.com.mx/modx/assets/img/revision2016/images/recetas/montaditos_fuerza_roja.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Croquetas de Vegetales","precio":"99","descripcion":"Fritura de carne de berenjena, papa y zanahoria rellos de cuajada y acompañados de Tatziki","foto":"https://www.hogarmania.com/archivos/201105/193-croquetas-de-verduras-y-queso-xl-668x400x80xX.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Palitos de Camote","precio":"49","descripcion":"Camotes a la francesa, acompañado de aderezo Tzatziki.","foto":"http://www.contigosalud.com/files/images/Palitos%20camote%20francesa.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Aros de Cebolla HK","precio":"89","descripcion":"5 aros de cebolla rellenos con pure de camote, guacamole y carne de berenjena y Empanizado con panco.","foto":"http://mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-2.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Palitos de Camote","precio":"49","descripcion":"Camotes a la francesa, acompañado de aderezo Tzatziki.","foto":"http://www.contigosalud.com/files/images/Palitos%20camote%20francesa.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Aros de Cebolla HK","precio":"89","descripcion":"5 aros de cebolla rellenos con pure de camote, guacamole y carne de berenjena y Empanizado con panco.","foto":"http://mylatinatable.com/wp-content/uploads/2016/01/foto-heroe-2.jpg","nutricional":""},
+        {"categoria":"Entree","titulo":"Croquetas de Vegetales","precio":"99","descripcion":"Fritura de carne de berenjena, papa y zanahoria rellos de cuajada y acompañados de Tatziki","foto":"https://www.hogarmania.com/archivos/201105/193-croquetas-de-verduras-y-queso-xl-668x400x80xX.jpg","nutricional":""}]
+    }
+
+    render() {
+        return (
             <div class="card-columns EmployeeCardColumn">
-            {renderPlates(Entrees)}
+                {renderPlates(Entrees, this.state.simon, this.imprimir)}
             </div>
-          );
-      }
-  }
+        );
+    }
 
-  const renderPlates = (platesList) => { //metodo a usar con la base
-    return platesList.map((plate) => {
-      return (
-  
-  
-        <div class="card EmployeeCard ">
+    imprimir = (i, estado) =>{
+        let entradass = this.entradas;
+        entradass[i].estado = estado;
+        this.setState({...state, entradas: entradass})
+    }
+}
+
+const renderPlates = (platesList, simon, imprimir) => { //metodo a usar con la base
+    return platesList.map((plate, i) => {
+        return (
+
+
+            <div class="card EmployeeCard " key={i}>
                 <div class="card-body">
                     <div>
 
@@ -100,24 +121,88 @@ var Entrees = [
                         <h2 id="InfoCliente" class="card-text">Teléfono: 94795544</h2>
                         <h2 id="InfoCliente" class="card-text">Dirección: Colonia quince de octubre, casa 103444, bloque xxxx, calle 23443</h2>
                         <p id="ComentarioCliente" class="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  {}
+                            Lorem ipsum dolor sitorem ipsum dolor sitorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  {}
                         </p>
                         <hr></hr>
 
 
                     </div>
 
-                    <div>
-                        <button>Pendiente</button>
-                    </div>
                     <div class="card-footer text-muted">
-                        Estado: {}
+                        <span id = "state">Estado: {plate.estado}</span>
+                        <button id = "btn" onClick={function () {
+                            if (plate.estado == "Pendiente") {
+                                console.log(platesList[i].estado);
+                                platesList[i].estado = "simon";
+                                console.log(platesList[i].estado);
+                                simon = "nomon";
+                                imprimir(i, "Terminado");
+                            } else {
+                                
+                            }
+                        }}>Cambiar a {returnState(plate.estado)}</button>
                     </div>
+
 
                 </div>
             </div>
-        
-  
-      );
+
+
+        );
     });
-  }
+}
+
+function returnState(state){
+    if(state == "Pendiente"){
+        return "Terminado";
+    } else{
+        return state;
+    }
+}
+
+class CardFooter extends React.Component {
+    render() {
+
+        if (this.props.plate.estado == "Pendiente") {
+
+            return (
+
+                <div>
+                    <span>Estado: {this.props.plate.estado}</span>
+                    <button onClick={function () {
+                        this.props.plate.estado = "Terminado"
+                    }}>Cambiar a Terminado</button>
+                </div>
+            );
+        } else {
+            return (
+
+                <div>
+                    <span>Estado: {this.props.plate.estado}</span>
+                    <button onClick={function () {
+                        this.props.plate.estado = "Pendiente"
+                    }}>Cambiar a Pendiente</button>
+                </div>
+
+            );
+        }
+    }
+}
+
+
+
+
+/*
+
+<div class="card-footer text-muted">
+                <span>Estado: {this.props.plate.estado}</span>
+                <button onClick={function () {
+                    if (this.props.plate.estado == "Pendiente") {
+                        this.props.plate.estado = "Terminado"
+                    } else {
+                        this.props.plate.estado = "Pendiente"
+                    }
+                }}>Cambiar a {returnState(this.props.plate.estado)}</button>
+            </div>
+
+*/
