1c6dbf2f91550c7999e84c71c8fa5344a6a32c6a
LuzHN <coldblitz@unitec.edu>
2018-07-25 17:41:06 -0600

    Se agrego la tabla con ordenes de empleado

--- imports/client/ui/menuempleado.js
+++ imports/client/ui/menuempleado.js
@@ -7,26 +7,26 @@ import {
 } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/menu.css';
-import {Orders} from '../../api/orders'
+import { Orders } from '../../api/orders'
 
 export default class MenuEmployee extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      orders: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: []
+        }
     }
-  }
-  componentDidMount() {
-    this.ordersTracker = Tracker.autorun(() => {
-      Meteor.subscribe('orders');
-      const orders = Orders.find().fetch();
-      this.setState({ orders });
-    });
-  }
+    componentDidMount() {
+        this.ordersTracker = Tracker.autorun(() => {
+            Meteor.subscribe('orders');
+            const orders = Orders.find().fetch();
+            this.setState({ orders });
+        });
+    }
 
-  componentWillUnmount() {
-    this.ordersTracker.stop();
-  }
+    componentWillUnmount() {
+        this.ordersTracker.stop();
+    }
 
     render() {
         return (
@@ -41,7 +41,7 @@ export default class MenuEmployee extends React.Component {
 
                 <section className="MenuEmployee" >
                     <h3 id="titulodeorden"></h3>
-                    <MenuEmployeeComponent orders={this.state.orders}/>
+                    <MenuEmployeeComponent orders={this.state.orders} />
                 </section>
 
                 <img id="ColorStrip" src="http://www.healthkitchen.hn/static/media/color-strip.9c28b147.svg" />
@@ -70,24 +70,136 @@ export default class MenuEmployee extends React.Component {
 class MenuEmployeeComponent extends React.Component {
     render() {
         return (
-            <div className="card-columns EmployeeCardColumn">
-                {renderPlates(this.props.orders, this.cambiarEstado, this.deleteCard)}
-            </div>
+
+            <table id="EmployeeTable" className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope="col">Nùmero Orden</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col">Telèfono</th>
+
+                        <th scope="col">Estado</th>
+                        <th scope="col">Ver más</th>
+                        <th scope="col">Ingresar</th>
+                        <th scope="col">Terminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderTable(this.props.orders)}
+                </tbody>
+            </table>
         );
     }
 }
-AgregarHeader = (cliente, id) =>{
-  return (
-    <h3>Cliente: {cliente} -- ID de Orden: {id}</h3>
-  )
+
+//{renderPlates(this.props.orders, this.cambiarEstado, this.deleteCard)}
+
+/*
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Nùmero Orden</th>
+      <th scope="col">Cliente</th>
+      <th scope="col">Telèfono</th>
+      <th scope="col">Direcciòn</th>
+      <th scope="col">Estado</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+*/
+
+
+AgregarHeader = (cliente, id) => {
+    return (
+        <h3>Cliente: {cliente} -- ID de Orden: {id}</h3>
+    )
 }
 
-const renderPlates = (platesList) => { //metodo a usar con la base
-  console.log(platesList)
+const renderTable = (platesList) => {
+
+    console.log(platesList);
+
 
     return platesList.map((plate) => {
-      // console.log(Meteor.users.findOne({_id: plate.userId}));
-      const user = Meteor.users.findOne({_id: plate.userId})
+
+        const user = Meteor.users.findOne({ _id: plate.userId })
+        if (plate.status == "") {
+            plate.status = "Pending";
+        }
+
+        if (plate.status == "Dispatched") {
+            //no mandar nada
+        } else {
+            return (
+                <tr key={plate._id}>
+                    <td>Orden X</td>
+                    <td>{plate.fecha}</td>
+                    <td>{user.profile.firstName + " " + user.profile.lastName}</td>
+                    <td>{user.profile.phoneNumber1}</td>
+                    <td>{plate.status}</td>
+                    <td>
+                        <button></button>
+                    </td>
+                    <td>
+                        <button id="btn-empleado" onClick={function () {
+                            if (plate.status == "Pending") {
+                                Meteor.call('orders.setInProgress', plate._id)
+                            }
+                        }}>Cambiar a Ingresado</button>
+                    </td>
+                    <td>
+                        <button id="btn-empleado" onClick={function () {
+                            if (plate.status == "InProgress" || plate.status == "Dispatched") {
+                                console.log("hola")
+                                Meteor.call('orders.setDispatched', plate._id)
+
+
+
+                                //document.getElementById(plate._id).remove();
+
+                            } else if (plate.status == "Pending") {
+                                alert("Primero tiene que estar ingresado.")
+                            }
+                        }}>Cambiar a Terminado</button>
+                    </td>
+                </tr>
+            )
+        }
+
+
+    });
+}
+
+
+const renderPlates = (platesList) => { //metodo a usar con la base
+
+    return platesList.map((plate) => {
+        // console.log(Meteor.users.findOne({_id: plate.userId}));
+        const user = Meteor.users.findOne({ _id: plate.userId })
         return (plate.products.map((product, i) => {
             return (
                 <div className="card EmployeeCard " key={i}>
@@ -107,9 +219,9 @@ const renderPlates = (platesList) => { //metodo a usar con la base
 
 
                         <div className="card-footer text-muted">
-                            <span className = "green">Estado: </span>
-                            <span className = "red">{plate.status}</span>
-                            <button className="ChangeState" id="ingresado" onClick= {function () {
+                            <span className="green">Estado: </span>
+                            <span className="red">{plate.status}</span>
+                            <button className="ChangeState" id="ingresado" onClick={function () {
                                 if (plate.status == "") {
                                     Meteor.call('orders.setInProgress', plate._id)
                                     this.document.getElementById("ingresado").setAttribute("disabled", "true");
@@ -126,7 +238,7 @@ const renderPlates = (platesList) => { //metodo a usar con la base
                     </div>
                 </div>
 
-                
+
             );
         }));
     });
