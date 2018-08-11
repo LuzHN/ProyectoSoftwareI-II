83490146a55239a43fd2d61aa7dff8f10f7871fd
josefernandez98 <jrfo_04@unitec.edu>
2018-08-10 21:27:53 -0600

    Se hiceron los modals de Agregar Administrador, Modificar Administrador y Agregar Usuario

--- imports/ui/EditarEmpleadoPage.js
+++ imports/ui/EditarEmpleadoPage.js
@@ -27,22 +27,47 @@ export default class editarEmpleadoPage extends React.Component {
     }
 
   onAgregar() {
-    var modal = document.getElementById('simpleModal');
+    var modal = document.getElementById('ModalAgregarEmpleado');
     modal.style.display = 'block';
   }
 
-  onEliminar() {
-    var modal = document.getElementById('simpleModal');
+  onModAdmin() {
+    var modal = document.getElementById('ModalModificarAdministrador');
     modal.style.display = 'block';
+  }
+
+  onAgregarUsuario() {
+    var modal = document.getElementById('ModalAgregarUsuario');
+    modal.style.display = 'block';
+  }
+
+  closeAgregarUsuario() {
+    var modal = document.getElementById('ModalAgregarUsuario');
+    modal.style.display = 'none';
+  }
+
+  onCloseModAdmin() {
+    var modal = document.getElementById('ModalModificarAdministrador');
+    modal.style.display = 'none';
+  }
+
+  onAgregarAdmin() {
+    var modal = document.getElementById('ModalAgregarAdministrador');
+    modal.style.display = 'block';
+  }
+
+  closeAdmin() {
+    var modal = document.getElementById('ModalAgregarAdministrador');
+    modal.style.display = 'none';
   }
 
   closeAgregar() {
-    var modal = document.getElementById('simpleModal');
+    var modal = document.getElementById('ModalAgregarEmpleado');
     modal.style.display = 'none';
   }
 
   closeModificar() {
-    var modal = document.getElementById('simpleModal2');
+    var modal = document.getElementById('ModalModificarEmpleado');
     modal.style.display = 'none';
   }
   
@@ -88,6 +113,9 @@ export default class editarEmpleadoPage extends React.Component {
         <div className="containerPrincipal">
             <div className="Buttons">
               <button className="botonAgregar" onClick={this.onAgregar.bind(this)}>Agregar Empleado</button>
+              <button className="botonAdmin" onClick={this.onAgregarAdmin.bind(this)}>Agregar Admin</button>
+              <button className="botonModAdmin" onClick={this.onModAdmin.bind(this)}>Modificar Admin</button>
+              <button className="botonAgregarUsuario" onClick={this.onAgregarUsuario.bind(this)}>Agregar Usuario</button>
             </div>
             
             <div className="searchBarDiv">   
@@ -102,8 +130,8 @@ export default class editarEmpleadoPage extends React.Component {
               </ul>
             </div>
             
-            {/*Modal*/}
-            <div className="modal" id="simpleModal">
+            {/*Modal Agregar Empleado*/}
+            <div className="modal" id="ModalAgregarEmpleado">
               <div className="modal-content">
                 {/* Header */}
                 <div className="modal-header">
@@ -181,8 +209,8 @@ export default class editarEmpleadoPage extends React.Component {
               </div>
             </div> {/*Termina MODAL AGREGAR*/}
 
-            {/*Modal*/}
-            <div id="simpleModal2" className="modal">
+            {/*Modal de Modificar Empleado*/}
+            <div id="ModalModificarEmpleado" className="modal">
               <div className="modal-content">
                 {/* Header */}
                 <div className="modal-header">
@@ -265,7 +293,251 @@ export default class editarEmpleadoPage extends React.Component {
                   {/* Footer */}
                   <div className="modal-footer"></div>
                   </div>
-                </div>{/*Termina MODAL MODIFICAR*/}
+                </div>{/*Termina MODAL MODIFICAR EMPLEADO*/}
+
+                {/*Modal Agregar ADMIN*/}
+                <div id="ModalAgregarAdministrador" className="modal">
+                  <div className="modal-content">
+                    {/* Header */}
+                    <div className="modal-header">
+                      <div className="modal-header-Btn">
+                        <span className="closeBtn" onClick={this.closeAdmin.bind(this)}>&times;</span>
+                      </div>
+                      <div className="modal-header-Name">
+                        <h2>Agregar Administrador</h2>
+                      </div>
+                    </div>
+                    {/* Body */}
+                    <div className="modal-body">
+                      <form className="agregarEmpleadoFormModal">
+                        <div>
+                          <div className="container1">
+                            <div className="box1">
+                              <p>
+                                <label>Email</label>
+                                <input id = "correo" maxLength='140' placeholder='Ingrese su correo.'  type="email"/>
+                              </p>
+                            </div>
+                          </div>
+                          <div className="container1">
+                            <div className="box1">
+                              <p>
+                                <label>Contraseña</label>
+                                <input placeholder='Ingrese su contraseña.'  type="password"/>
+                              </p>
+                            </div>
+                            <div className="box2">
+                              <p>
+                                <label>Confirmar Contraseña</label>
+                                <input placeholder='Confirmar contraseña.' ref="confirmPassword" type="password"/>
+                              </p>
+                            </div>
+                          </div>
+                          <div className="container1">
+                            <div className="box1">
+                              <p>
+                                <label>Primer Nombre</label>
+                                <input maxLength='140' placeholder='Ingrese su primer nombre.' ref="firstName" type="text" />
+                              </p>
+                            </div>
+                            <div className="box2">
+                              <p>
+                                <label>Apellido</label>
+                                <input maxLength='140' placeholder='Ingrese su apellido.' ref="lastName" type="text"/>
+                              </p>
+                            </div>
+                          </div>
+                          <div className="container1">
+                            <div className="box1">
+                              <p>
+                                <label>Número de Teléfono</label>
+                                <InputMask id = "numero" mask="9999-9999" placeholder='Ingrese su número de teléfono.' ref="phoneNumber"/>
+                              </p>
+                            </div>
+                          </div>
+                          <div className="container1">
+                            <div className="box1">
+                              <p>
+                                <label>Dirección</label>
+                                <textarea maxLength='140' placeholder='Ingrese su dirección.' ref="address" rows="5"></textarea>
+                              </p>
+                            </div>
+                          </div>
+                          <p>
+                            <button className="confirmarAdd" >Confirmar</button>
+                          </p>
+                        </div>
+                      </form>
+                    </div>
+                    {/* Footer */}
+                    <div className="modal-footer"></div>
+                  </div>
+                </div> {/*Termina MODAL AGREGAR ADMIN*/}
+
+                {/*Modal de Modificar Administrador*/}
+                <div id="ModalModificarAdministrador" className="modal">
+                  <div className="modal-content">
+                    {/* Header */}
+                    <div className="modal-header">
+                        <div className="modal-header-Btn">
+                            <span className="closeBtn" onClick={this.onCloseModAdmin.bind(this)}>&times;</span>
+                        </div>
+                        <div className="modal-header-Name">
+                          <h2>Modificar Administrador</h2>
+                        </div>
+                    </div>
+                    {/* Body */}
+                    <div className="modal-body">
+                      <form  className="agregarEmpleadoFormModal">
+                        <div>
+                            <div className = "container1">
+                              <div className = "box1">
+                                <p>
+                                  <label>Primer Nombre</label>
+                                  <input id = "firstNameId" maxLength='140' placeholder='Ingrese primer nombre.' ref = "firstName"/>
+                                </p>
+                              </div>
+                              <div className="box2">
+                                <p>
+                                  <label>Apellido</label>
+                                  <input maxLength='140' placeholder='Ingrese su apellido.' ref="lastName" type="text"/>
+                                </p>
+                              </div>
+                            </div>
+                            <div className="container1">
+                              <div className="box1">
+                                <p>
+                                  <label>*Teléfono 1</label>
+                                  <InputMask mask="9999-9999" placeholder='Ingrese su teléfono.' ref="phoneNumber1"/>
+                                  <label>Teléfono 3</label>
+                                  <InputMask mask="9999-9999" placeholder='Ingrese su teléfono.' ref="phoneNumber3"/>
+                                </p>
+                              </div>
+                              <div className="box2">
+                                <p>
+                                  <label>Teléfono 2</label>
+                                  <InputMask mask="9999-9999" placeholder='Ingrese su teléfono.' ref="phoneNumber2"/>
+                                  <label>Teléfono 4</label>
+                                  <InputMask mask="9999-9999" placeholder='Ingrese su teléfono.' ref="phoneNumber4"/>
+                                </p>
+                              </div>
+                            </div>
+                            <div className="container1">
+                              <div className="box1">
+                                <p>
+                                  <label>*Dirección 1</label>
+                                  <textarea id = "direction1TextArea" maxLength='140' placeholder='Ingrese su dirección.' ref="address1" rows="5"/>
+                                  <label>Dirección 3</label>
+                                  <textarea id = "direction3TextArea" maxLength='140' placeholder='Ingrese su dirección.' ref="address3" rows="5"/>
+                                </p>
+                              </div>
+                              <div className="box2">
+                                <p>
+                                  <label>Dirección 2</label>
+                                  <textarea id = "direction2TextArea" maxLength='140' placeholder='Ingrese su dirección.' ref="address2" rows="5"/>
+                                  <label>Dirección 4</label>
+                                  <textarea id = "direction4TextArea" maxLength='140' placeholder='Ingrese su dirección.' ref="address4" rows="5"/>
+                                </p>
+                              </div>
+                            </div>  
+                            <div className="container1">
+                              <div className="box1">
+                                <p>
+                                  <button className = "confirmarModificar" >Confirmar Cambios</button>
+                                </p>
+                              </div>
+                              <div className="box2">
+                                <p>
+                                  <button className = "confirmarDesactivar" >Desactivar Administrador</button>
+                                </p>
+                              </div>
+                            </div>     
+                          </div>       
+                        </form>
+                      </div>
+                      {/* Footer */}
+                      <div className="modal-footer"></div>
+                      </div>
+                    </div>{/*Termina MODAL MODIFICAR Administrador*/}
+
+                    {/*Modal Agregar Usuario*/}
+                    <div id="ModalAgregarUsuario" className="modal">
+                      <div className="modal-content">
+                        {/* Header */}
+                        <div className="modal-header">
+                          <div className="modal-header-Btn">
+                            <span className="closeBtn" onClick={this.closeAgregarUsuario.bind(this)}>&times;</span>
+                          </div>
+                          <div className="modal-header-Name">
+                            <h2>Agregar Usuario</h2>
+                          </div>
+                        </div>
+                        {/* Body */}
+                        <div className="modal-body">
+                          <form className="agregarEmpleadoFormModal">
+                            <div>
+                              <div className="container1">
+                                <div className="box1">
+                                  <p>
+                                    <label>Email</label>
+                                    <input id = "correo" maxLength='140' placeholder='Ingrese su correo.'  type="email"/>
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="container1">
+                                <div className="box1">
+                                  <p>
+                                    <label>Contraseña</label>
+                                    <input placeholder='Ingrese su contraseña.'  type="password"/>
+                                  </p>
+                                </div>
+                                <div className="box2">
+                                  <p>
+                                    <label>Confirmar Contraseña</label>
+                                    <input placeholder='Confirmar contraseña.' ref="confirmPassword" type="password"/>
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="container1">
+                                <div className="box1">
+                                  <p>
+                                    <label>Primer Nombre</label>
+                                    <input maxLength='140' placeholder='Ingrese su primer nombre.' ref="firstName" type="text" />
+                                  </p>
+                                </div>
+                                <div className="box2">
+                                  <p>
+                                    <label>Apellido</label>
+                                    <input maxLength='140' placeholder='Ingrese su apellido.' ref="lastName" type="text"/>
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="container1">
+                                <div className="box1">
+                                  <p>
+                                    <label>Número de Teléfono</label>
+                                    <InputMask id = "numero" mask="9999-9999" placeholder='Ingrese su número de teléfono.' ref="phoneNumber"/>
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="container1">
+                                <div className="box1">
+                                  <p>
+                                    <label>Dirección</label>
+                                    <textarea maxLength='140' placeholder='Ingrese su dirección.' ref="address" rows="5"></textarea>
+                                  </p>
+                                </div>
+                              </div>
+                              <p>
+                                <button className="confirmarAdd" >Confirmar</button>
+                              </p>
+                            </div>
+                          </form>
+                        </div>
+                        {/* Footer */}
+                        <div className="modal-footer"></div>
+                      </div>
+                    </div> {/*Termina MODAL AGREGAR USUARIO*/}
             </div>
         </div>
       );
@@ -276,7 +548,7 @@ const renderUser = (users) => {
     return users.map((user) => {
         return (
             <li  onClick={function () {
-                var modal = document.getElementById('simpleModal2');
+                var modal = document.getElementById('ModalModificarEmpleado');
                 modal.style.display = 'block';x
             }} className="collection-item" key={user._id}>
                 <a href="#"  className="hrefNombre">{user.profile.firstName} {user.profile.lastName}</a>
