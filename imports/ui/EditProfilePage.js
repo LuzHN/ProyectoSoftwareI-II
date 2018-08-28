a69fd7f9ad5ce306211db84a1a131f29d9d00a78
hrmendoza96 <hrmendoza96@unitec.edu>
2018-08-28 00:39:46 -0600

    Llena Inputs, permite editarlos, correcciones realizadas

--- imports/ui/EditProfilePage.js
+++ imports/ui/EditProfilePage.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import { disconnect } from 'cluster';
 import PropTypes from 'prop-types';
 import { Router, Route, browserHistory } from 'react-router';
@@ -10,23 +10,11 @@ import InputMask from 'react-input-mask';
 import { createContainer } from 'meteor/react-meteor-data';
 import '../client/styles/EditProfile.css';
 
-let profile;
-let firstName;
-let lastName;
-let phoneNumber1;
-let phoneNumber2;
-let phoneNumber3;
-let phoneNumber4;
-let address1;
-let address2;
-let address3;
-let address4;
-
 
 class EditProfilePage extends React.Component {
   constructor(props) {
     super(props);
-    
+
   }
 
   handleSubmit(e) {
@@ -34,11 +22,69 @@ class EditProfilePage extends React.Component {
   }
 
   componentDidMount() {
-    
+
+    let user;
+    let firstName;
+    let lastName;
+    let phoneNumber1;
+    let phoneNumber2;
+    let phoneNumber3;
+    let phoneNumber4;
+    let address1;
+    let address2;
+    let address3;
+    let address4;
+    /*El SetTimeout se utiliza para darle tiempo a Meteor de cargar la informacion del user*/
+    setTimeout(() => {
+      user = Meteor.user() ? Meteor.user().profile : '';
+      console.log(user);
+      firstName = user.firstName;
+      lastName = user.lastName;
+      phoneNumber1 = user.phoneNumber1;
+      phoneNumber2 = user.phoneNumber2;
+      phoneNumber3 = user.phoneNumber3;
+      phoneNumber4 = user.phoneNumber4;
+      address1 = user.address1;
+      address2 = user.address2;
+      address3 = user.address3;
+      address4 = user.address4;
+      // console.log("firstName: " + firstName);
+      // console.log("lastName: " + lastName);
+      // console.log("phoneNumber1: " + phoneNumber1);
+      // console.log("phoneNumber2: " + phoneNumber2);
+      // console.log("phoneNumber3: " + phoneNumber3);
+      // console.log("phoneNumber4: " + phoneNumber4);
+      // console.log("address1: " + address1);
+      // console.log("address2: " + address2);
+      // console.log("address3: " + address3);
+      // console.log("address4: " + address4);
+      this.refs.firstName.value = firstName;
+      this.refs.lastName.value = lastName;
+      /* Se aplica el jquery para que se visualicen los valores antes que los cargue con ref 
+      * (ya que con ref solo cargan hasta que se le de click al InputMask ) 
+      * */
+      $('#phoneNumber1').val(phoneNumber1);
+      $('#phoneNumber2').val(phoneNumber2);
+      $('#phoneNumber3').val(phoneNumber3);
+      $('#phoneNumber4').val(phoneNumber4);
+      this.refs.phoneNumber1.value = phoneNumber1;
+      this.refs.phoneNumber2.value = phoneNumber2;
+      this.refs.phoneNumber3.value = phoneNumber3;
+      this.refs.phoneNumber4.value = phoneNumber4;
+      this.refs.address1.value = address1;
+      this.refs.address2.value = address2;
+      this.refs.address3.value = address3;
+      this.refs.address4.value = address4;
+
+    }, 1000);
+
+
+
+
   }
 
   componentWillUnmount() {
-    
+
   }
 
   onSubmit() {
@@ -145,22 +191,19 @@ class EditProfilePage extends React.Component {
       }
     }
 
-    let profile = {
-      firstName,
-      lastName,
-      phoneNumber1,
-      phoneNumber2,
-      phoneNumber3,
-      phoneNumber4,
-      address1,
-      address2,
-      address3,
-      address4
-    }
-
+   
     if (!validator) {
       Meteor.call('user.updateProfileSelf', {
-        profile
+        firstName,
+        lastName,
+        phoneNumber1,
+        phoneNumber2,
+        phoneNumber3,
+        phoneNumber4,
+        address1,
+        address2,
+        address3,
+        address4
       });
       toastr.success('Perfil Modificado.');
       this.props.history.push('/');
@@ -184,41 +227,10 @@ class EditProfilePage extends React.Component {
     modal.style.display = 'none';
   }
 
-  loadPrint() {
-    profile = Meteor.user() ? Meteor.user().profile : '';
-    // console.log(profile);
-    firstName = Meteor.user() ? Meteor.user().profile.profile.firstName : '';
-    // console.log("firstName: " + firstName);
-    lastName = Meteor.user() ? Meteor.user().profile.profile.lastName : '';
-    // console.log("lastName: " + lastName);
-    phoneNumber1 = Meteor.user() ? Meteor.user().profile.profile.phoneNumber1 : '';
-    // console.log("phoneNumber1: " + phoneNumber1);
-    phoneNumber2 = Meteor.user() ? Meteor.user().profile.profile.phoneNumber2 : '';
-    // console.log("phoneNumber2: " + phoneNumber2);
-    phoneNumber3 = Meteor.user() ? Meteor.user().profile.profile.phoneNumber3 : '';
-    // console.log("phoneNumber3: " + phoneNumber3);
-    phoneNumber4 = Meteor.user() ? Meteor.user().profile.profile.phoneNumber4 : '';
-    // console.log("phoneNumber4: " + phoneNumber4);
-    address1 = Meteor.user() ? Meteor.user().profile.profile.address1 : '';
-    // console.log("address1: " + address1);
-    address2 = Meteor.user() ? Meteor.user().profile.profile.address2 : '';
-    // console.log("address2: " + address2);
-    address3 = Meteor.user() ? Meteor.user().profile.profile.address3 : '';
-    // console.log("address3: " + address3);
-    address4 = Meteor.user() ? Meteor.user().profile.profile.address4 : '';
-    // console.log("address4: " + address4);
-    
-  }
-
-  // loadInfo(){
-  //   $('#address1').text(address1);
-  // }
-
   render() {
     /**
      * TODO: Importar de la DB la info actual del usuario
      */
-    this.loadPrint();
     return (
       <div className="principalDiv">
         <form
@@ -230,11 +242,11 @@ class EditProfilePage extends React.Component {
           <div className="containerBox">
             <div className="leftContainerBox">
               <label>Nombre</label>
-              <input ref="firstName" id="firstName" defaultValue={firstName} maxLength="140" placeholder="Ingrese primer nombre." />
+              <input ref="firstName" id="firstName" maxLength="140" placeholder="Ingrese primer nombre." />
             </div>
             <div className="rightContainerBox">
               <label>Apellido</label>
-              <input ref="lastName" id="lastName" defaultValue={lastName} maxLength="140" placeholder="Ingrese apellido." />
+              <input ref="lastName" id="lastName" maxLength="140" placeholder="Ingrese apellido." />
             </div>
           </div>
           {/*First Name and Last Name inputs and labels ends here.*/}
@@ -275,11 +287,11 @@ class EditProfilePage extends React.Component {
           <div className="containerBox">
             <div className="leftContainerBox">
               <label>*Teléfono 1</label>
-              <InputMask ref="phoneNumber1" value={phoneNumber1} id="phoneNumber1" mask="9999-9999" placeholder="Ingrese su teléfono." />
+              <InputMask ref="phoneNumber1" id="phoneNumber1" mask="9999-9999" placeholder="Ingrese su teléfono." />
             </div>
             <div className="rightContainerBox">
               <label>Teléfono 2</label>
-              <InputMask ref="phoneNumber2" value={phoneNumber2} id="phoneNumber2" mask="9999-9999" placeholder="Ingrese su teléfono." />
+              <InputMask ref="phoneNumber2" id="phoneNumber2" mask="9999-9999" placeholder="Ingrese su teléfono." />
             </div>
           </div>
           {/*First two phone numbers end here.*/}
@@ -288,11 +300,11 @@ class EditProfilePage extends React.Component {
           <div className="containerBox">
             <div className="leftContainerBox">
               <label>Teléfono 3</label>
-              <InputMask ref="phoneNumber3" value={phoneNumber3} id="phoneNumber3" mask="9999-9999" placeholder="Ingrese su teléfono." />
+              <InputMask ref="phoneNumber3" id="phoneNumber3" mask="9999-9999" placeholder="Ingrese su teléfono." />
             </div>
             <div className="rightContainerBox">
               <label>Teléfono 4</label>
-              <InputMask ref="phoneNumber4" value={phoneNumber4} id="phoneNumber4" mask="9999-9999" placeholder="Ingrese su teléfono." />
+              <InputMask ref="phoneNumber4" id="phoneNumber4" mask="9999-9999" placeholder="Ingrese su teléfono." />
             </div>
           </div>
           {/*Last two phone numbers end here.*/}
@@ -301,11 +313,11 @@ class EditProfilePage extends React.Component {
           <div className="containerBox">
             <div className="leftContainerBox">
               <label>*Dirección 1</label>
-              <textarea  ref="address1" id="address1" value={address1} maxLength="140" rows="5" placeholder="Ingrese su dirección."></textarea>
+              <textarea ref="address1" id="address1" maxLength="140" rows="5" placeholder="Ingrese su dirección."></textarea>
             </div>
             <div className="rightContainerBox">
               <label>Dirección 2</label>
-              <textarea ref="address2" value={address2} id="address2" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
+              <textarea ref="address2" id="address2" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
             </div>
           </div>
           {/*First two directions text areas end here.*/}
@@ -314,11 +326,11 @@ class EditProfilePage extends React.Component {
           <div className="containerBox">
             <div className="leftContainerBox">
               <label>Dirección 3</label>
-              <textarea ref="address3" value={address3} id="address3" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
+              <textarea ref="address3" id="address3" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
             </div>
             <div className="rightContainerBox">
               <label>Dirección 4</label>
-              <textarea ref="address4" value={address4} id="address4" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
+              <textarea ref="address4" id="address4" maxLength="140" rows="5" placeholder="Ingrese su dirección." />
             </div>
           </div>
           {/*Last two directions text areas end here.*/}
